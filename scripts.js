2a17d6eba4ff92121b407130b39ed93308af0e4c
Deniz Gould <deniz.gould@live.com>
2020-11-01 06:58:20 -0400

    fixxed error on display of the message div before action happened

--- scripts.js
+++ scripts.js
@@ -6,9 +6,8 @@ window.addEventListener("load",
         var submit = document.getElementsByClassName("btn")[1];
         // console.log(submit)
         var text = document.getElementById("email");
-        console.log(text)
+        // console.log(text)
         var message = document.querySelector('.message');
-        message.innerHTML = "Email Address"
         // console.log(message)
 
         submit.addEventListener('click', function(e){
@@ -16,8 +15,8 @@ window.addEventListener("load",
             console.log("button clicked")
             var email = text.value
             console.log(email)
-            if (email === " "){
-                email.innerHTML = "Please Enter a Valid Email Address."
+            if (email === ""){
+                message.innerHTML = "Please Enter a Valid Email Address."
             }else{
             message.innerHTML = `Thank You! Your email address ${email} has been added to our mailing list!`
             document.getElementById("email").value = null;
